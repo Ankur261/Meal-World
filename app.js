af0e2d80db514fa595e89d74dd90c23a75d676ef
Ankur261 <ankuragrawal911@gmail.com>
2023-09-14 13:01:38 +0530

    add cards with meal image and meal name

--- app.js
+++ app.js
@@ -1,11 +1,5 @@
-const fetchMealsFromApi = async (url, value) => {
-    const response = await fetch(`${url + value}`);
-    const meals = await response.json();
-    return meals;
-}
 
-const response = fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=chicken`);
-const meals =  response.json();
+var searchInput = document.getElementById('search-input') ;
 
 /**
  * showMealList - function to show meal list based on search input
@@ -17,18 +11,16 @@ const meals =  response.json();
  * for each meal. This HTML template is then added to the DOM.
  */
 
-async function showMealList() {
-    const list = JSON.parse(localStorage.getItem(dbObjectFavList));
-    const inputValue = document.getElementById("search-input").value;
-    const url = "https://www.themealdb.com/api/json/v1/1/search.php?s=";
-    const mealsData = await fetchMealsFromApi(url, inputValue);
+async function MealList(searchKeyword) {
+    const searchMelaUrl = "https://www.themealdb.com/api/json/v1/1/search.php?s=" ;
+    const response = await fetch(`${searchMelaUrl}${searchKeyword}`).then(response => response.json()).catch(error => console.error("Error: ", error));
     let html = '';
-    if (mealsData.meals) {
-        html = mealsData.meals.map(element => {
-
-            return `         
-            <div class="card">
-            <div class="card-top"  onclick="showMealDetails(${element.idMeal}, '${inputValue}')">
+   // ${isFav(list, element.idMeal) ? 'active' : ''}
+    if (response.meals) {
+        html = response.meals.map(element => {
+            return `
+            <div class="card" onClick = "${mealDetailPage(element)}">
+            <div class="card-top" >
                 <div class="dish-photo" >
                     <img src="${element.strMealThumb}" alt="">
                 </div>
@@ -36,16 +28,13 @@ async function showMealList() {
                     ${element.strMeal}
                 </div>
                 <div class="dish-details">
-                    ${truncate(element.strInstructions, 50)}
-                    
-                    <span class="button" onclick="showMealDetails(${element.idMeal}, '${inputValue}')">Know More</span>
                  
                 </div>
             </div>
             <div class="card-bottom">
                 <div class="like">
 
-                <i class="fa-solid fa-heart ${isFav(list, element.idMeal) ? 'active' : ''} " onclick="addRemoveToFavList(${element.idMeal})"></i>
+                <img src="./assets/heart-regular" class="" onclick="addRemoveToFavList(${element.idMeal})"></i>
                 
                 </div>
                 <div class="play">
@@ -57,7 +46,126 @@ async function showMealList() {
         </div>
             `
         }).join('');
-        document.getElementById('cards-holder').innerHTML = html;
+        document.getElementById('result-section').innerHTML = html;
     }
 }
+
+
+
+async function mealDetailPage(mealDetails) {
+    const list = JSON.parse(localStorage.getItem(dbObjectFavList));
+    let html = ''
+    if (mealDetails != null) {
+        html = `
+        <div class="container remove-top-margin">
+
+            <div class="header hide">
+                <div class="title">
+                    Let's Eat Something New
+                </div>
+            </div>
+            <div class="fixed" id="search-bar">
+                <div class="icon">
+                    <i class="fa-solid fa-search "></i>
+                </div>
+                <div class="new-search-input">
+                    <form onkeyup="showMealList()">
+                        <input id="search-input" type="text" placeholder="Search food, receipe" />
+                    </form>
+                </div>
+            </div>
+        </div>
+        <div class="item-details">
+        <div class="item-details-left">
+        <img src="  ${mealDetails.meals[0].strMealThumb}" alt="">
+    </div>
+    <div class="item-details-right">
+        <div class="item-name">
+            <strong>Name: </strong>
+            <span class="item-text">
+            ${mealDetails.meals[0].strMeal}
+            </span>
+         </div>
+        <div class="item-category">
+            <strong>Category: </strong>
+            <span class="item-text">
+            ${mealDetails.meals[0].strCategory}
+            </span>
+        </div>
+        <div class="item-ingrident">
+            <strong>Ingrident: </strong>
+            <span class="item-text">
+            ${mealDetails.meals[0].strIngredient1},${mealDetails.meals[0].strIngredient2},
+            ${mealDetails.meals[0].strIngredient3},${mealDetails.meals[0].strIngredient4}
+            </span>
+        </div>
+        <div class="item-instruction">
+            <strong>Instructions: </strong>
+            <span class="item-text">
+            ${mealDetails.meals[0].strInstructions}
+            </span>
+        </div>
+        <div class="item-video">
+            <strong>Video Link:</strong>
+            <span class="item-text">
+            <a href="${mealDetails.meals[0].strYoutube}">Watch Here</a>
+          
+            </span>
+            <div id="like-button" onclick="addRemoveToFavList(${mealDetails.meals[0].idMeal})"> 
+             ${isFav(list, mealDetails.meals[0].idMeal) ? 'Remove From Favourite' : 'Add To Favourite'} </div>
+        </div>
+    </div>
+</div> 
+        <div class="card-name">
+        Related Items
+    </div>
+    <div id="cards-holder" class=" remove-top-margin ">`
+    }
+    if( mealList.meals!=null){
+        html += mealList.meals.map(element => {
+            return `       
+            <div class="card">
+                <div class="card-top"  onclick="showMealDetails(${element.idMeal}, '${searchInput}')">
+                    <div class="dish-photo" >
+                        <img src="${element.strMealThumb}" alt="">
+                    </div>
+                    <div class="dish-name">
+                        ${element.strMeal}
+                    </div>
+                    <div class="dish-details">
+                        ${truncate(element.strInstructions, 50)}
+                        <span class="button" onclick="showMealDetails(${element.idMeal}, '${searchInput}')">Know More</span>
+                    </div>
+                </div>
+                <div class="card-bottom">
+                    <div class="like">
+                       
+                        <i class="fa-solid fa-heart ${isFav(list, element.idMeal) ? 'active' : ''} " 
+                        onclick="addRemoveToFavList(${element.idMeal})"></i>
+                    </div>
+                    <div class="play">
+                        <a href="${element.strYoutube}">
+                            <i class="fa-brands fa-youtube"></i>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        `
+        }).join('');
+    }
+
+  
+    html = html + '</div>';
+
+    document.getElementById('meal-details').innerHTML = html;
+}
+
+
+
+searchInput.addEventListener("input", (e) => {
+    const inputValue = searchInput.value;
+    MealList(inputValue) ;
+   // console.log(MealList(inputValue)) ;
+})
+
 
