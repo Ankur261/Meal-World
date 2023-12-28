d81e90f65d9092273bdcfdd770251e751fd187e3
Ankur261 <ankuragrawal911@gmail.com>
2023-12-28 19:21:38 +0530

    added

--- app.js
+++ app.js
@@ -3,10 +3,13 @@ var searchInput = document.getElementById('search-input');
 const toggleButton = document.getElementById("toggle-sidebar");
 
 
+
+
+
 /**
  * Check and initialize the local storage items for favorite list and last input
  */
-const dbObjectFavList = "favouritesList";
+export const dbObjectFavList = "favouritesList";
  if (localStorage.getItem(dbObjectFavList) == null) {
     localStorage.setItem(dbObjectFavList, JSON.stringify([]));
 }
@@ -30,11 +33,14 @@ function isFav(list, id) {
 }
 
 
-toggleButton.addEventListener("click", function () {
-    showFavMealList();
-    sidebar.classList.toggle("show");
-    flexBox.classList.toggle('shrink');
-});
+if (toggleButton) {
+    toggleButton.addEventListener("click", function () {
+        showFavMealList();
+        sidebar.classList.toggle("show");
+    });
+} else {
+    console.error("Toggle button not found!");
+}
 
 
 /**
@@ -46,8 +52,9 @@ toggleButton.addEventListener("click", function () {
  * If it exists, it removes it from the list, otherwise it adds it to the list. It then updates the local storage and updates the UI.
  */
 
-function addRemoveToFavList(id) {
-    const detailsPageLikeBtn = document.getElementById('like-button');
+
+ export function addRemoveToFavList(id, likeButton) {
+    const list = JSON.parse(localStorage.getItem(dbObjectFavList));
     let db = JSON.parse(localStorage.getItem(dbObjectFavList));
     let ifExist = false;
     for (let i = 0; i < db.length; i++) {
@@ -65,14 +72,15 @@ function addRemoveToFavList(id) {
     }
 
     localStorage.setItem(dbObjectFavList, JSON.stringify(db));
-    if (detailsPageLikeBtn != null) {
-        detailsPageLikeBtn.innerHTML = isFav(db, id) ? 'Remove From Favourite' : 'Add To Favourite';
-    }
 
-    showMealList();
+
+    likeButton.innerHTML = isFav(list, id) ? 'Add To Favourite' : 'Remove From Favourite';
+
+    MealList(searchInput.value);
     showFavMealList();
-    updateTask();
+    console.log('this running')
 }
+
 
 
 
@@ -82,11 +90,30 @@ function noMealToShow() {
         resultSection.innerHTML = '<div id="no-meal-to-show"> No Meal to Show Here! </div>'
     }
     else {
-        MealList(searchInput.value)
-        console.log(searchInput.value) ;
+        MealList(searchInput.value) ;
     }
 }
 
+
+/**
+ * 
+ * This function adds an event listener to the toggle button that when clicked, it calls the showFavMealList function and adds or removes the "show" class to the sidebar element and "shrink" class to the flexBox element, respectively.
+ * @event toggleButton - The button element that when clicked, triggers the event listener.
+ * @function showFavMealList - The function that is called when the toggle button is clicked. It populates the fav element with the list of favorite meals.
+ * @element sidebar - The sidebar element that has the "show" class added or removed.
+ * @element flexBox - The flexbox element that has the "shrink" class added or removed.
+*/
+
+
+
+
+
+
+const fetchMealsFromApi = async (url, value) => {
+    const response = await fetch(`${url + value}`);
+    const meals = await response.json();
+    return meals;
+}
 
 
 /**
@@ -105,104 +132,188 @@ This function is used to show all the meals which are added to the favourite lis
 
 showFavMealList()
 */
-async function showFavMealList() {
+export async function showFavMealList() {
+    const list = JSON.parse(localStorage.getItem(dbObjectFavList));
     let favList = JSON.parse(localStorage.getItem(dbObjectFavList));
     let url = "https://www.themealdb.com/api/json/v1/1/lookup.php?i=";
-    let html = "";
-
-    if (favList.length == 0) {
-        html = `<div class="fav-item nothing"> <h1> 
-        Nothing To Show.....</h1> </div>`
+    const favSection = document.getElementById('fav');
+    favSection.innerHTML = ''; // Clear existing content
+    
+    if (favList.length === 0) {
+        // Create a div with the message "Nothing To Show....."
+        const nothingDiv = document.createElement('div');
+        nothingDiv.classList.add('fav-item', 'nothing');
+        const h1 = document.createElement('h1');
+        h1.textContent = 'Nothing To Show.....';
+        nothingDiv.appendChild(h1);
+    
+        // Append the created div to the favSection
+        favSection.appendChild(nothingDiv);
     } else {
         for (let i = 0; i < favList.length; i++) {
             const favMealList = await fetchMealsFromApi(url, favList[i]);
             if (favMealList.meals[0]) {
-                let element = favMealList.meals[0];
-                html += `
-                <div class="fav-item" onclick="showMealDetails(${element.idMeal},'${generateOneCharString()}')">
-
-              
-                <div class="fav-item-photo">
-                    <img src="${element.strMealThumb}" alt="">
-                </div>
-                <div class="fav-item-details">
-                    <div class="fav-item-name">
-                        <strong>Name: </strong>
-                        <span class="fav-item-text">
-                           ${element.strMeal}
-                        </span>
-                    </div>
-                    <div id="fav-like-button" onclick="addRemoveToFavList(${element.idMeal})">
-                        Remove
-                    </div>
-
-                </div>
-
-            </div>               
-                `
+                const element = favMealList.meals[0];
+    
+                // Create main container div
+                const favItemContainer = document.createElement('div');
+                favItemContainer.classList.add('fav-item');
+    
+                // Create fav-item-photo div
+                const favItemPhoto = document.createElement('div');
+                favItemPhoto.classList.add('fav-item-photo');
+                const img = document.createElement('img');
+                img.src = element.strMealThumb;
+                img.alt = '';
+                favItemPhoto.appendChild(img);
+    
+                // Create fav-item-details div
+                const favItemDetails = document.createElement('div');
+                favItemDetails.classList.add('fav-item-details');
+    
+                // Create fav-item-name div
+                const favItemName = document.createElement('div');
+                favItemName.classList.add('fav-item-name');
+                const strong = document.createElement('strong');
+                strong.textContent = 'Name:';
+                const span = document.createElement('span');
+                span.classList.add('fav-item-text');
+                span.textContent = element.strMeal;
+                favItemName.appendChild(strong);
+                favItemName.appendChild(span);
+    
+                // Create fav-like-button div with onclick event
+                const favLikeButton = document.createElement('div');
+                favLikeButton.id = 'fav-like-button';
+                favLikeButton.textContent = 'Remove';
+                favLikeButton.addEventListener('click', () => addRemoveToFavList(element.idMeal, favLikeButton));
+    
+                // Append favItemName and favLikeButton to favItemDetails
+                favItemDetails.appendChild(favItemName);
+                favItemDetails.appendChild(favLikeButton);
+    
+                // Append favItemPhoto and favItemDetails to favItemContainer
+                favItemContainer.appendChild(favItemPhoto);
+                favItemContainer.appendChild(favItemDetails);
+    
+                // Append favItemContainer to favSection
+                favSection.appendChild(favItemContainer);
             }
         }
     }
-    document.getElementById('fav').innerHTML = html;
+    
 }
 
 
 
 
 
-var MealList = async function (searchKeyword) {
+export var MealList = async function (searchKeyword) {
+    const list = JSON.parse(localStorage.getItem(dbObjectFavList));
     const searchMelaUrl = "https://www.themealdb.com/api/json/v1/1/search.php?s=";
     const response = await fetch(searchMelaUrl + searchKeyword).then(response => response.json()).catch(error => console.error("Error: ", error));
-    let html = '';
     if (response.meals) {
-        html = response.meals.map(element => {
-            return `
-            
-            <div class="card" >
-            <a href="./meal-detail-page.html?id=${element.idMeal}" target="blank">
-            <div class="card-top" >
-                <div class="dish-photo" >
-                    <img src="${element.strMealThumb}" alt="">
-                </div>
-                <div class="dish-name">
-                    ${element.strMeal}
-                </div>
-                <div class="dish-details">
-                 Catergory: ${element.strCategory}
-                 <br>
-                 Native: ${element.strArea}
-                 <br>
-                 Tags: ${element.strTags}
-                </div>
-            </div>
-            </a>
-            <div class="card-bottom">
-                <div id="like" >
-                <svg xmlns="http://www.w3.org/2000/svg" height="16" width="16" viewBox="0 0 512 512"><!--!Font Awesome Free 6.5.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2023 Fonticons, Inc.--><path fill="#e71313" d="M47.6 300.4L228.3 469.1c7.5 7 17.4 10.9 27.7 10.9s20.2-3.9 27.7-10.9L464.4 300.4c30.4-28.3 47.6-68 47.6-109.5v-5.8c0-69.9-50.5-129.5-119.4-141C347 36.5 300.6 51.4 268 84L256 96 244 84c-32.6-32.6-79-47.5-124.6-39.9C50.5 55.6 0 115.2 0 185.1v5.8c0 41.5 17.2 81.2 47.6 109.5z"/></svg>
-                </div>
-                <div class="play">
-                <a href="${element.strYoutube}" target="blank">
-                <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 576 512"><!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><style>svg{fill:#ea0606}</style><path d="M549.655 124.083c-6.281-23.65-24.787-42.276-48.284-48.597C458.781 64 288 64 288 64S117.22 64 74.629 75.486c-23.497 6.322-42.003 24.947-48.284 48.597-11.412 42.867-11.412 132.305-11.412 132.305s0 89.438 11.412 132.305c6.281 23.65 24.787 41.5 48.284 47.821C117.22 448 288 448 288 448s170.78 0 213.371-11.486c23.497-6.321 42.003-24.171 48.284-47.821 11.412-42.867 11.412-132.305 11.412-132.305s0-89.438-11.412-132.305zm-317.51 213.508V175.185l142.739 81.205-142.739 81.201z"/></svg>
-                </a>
-                </div>
-            </div>
-        </div>
-        
-            `
-        }).join('');
+        const resultSection = document.getElementById('result-section');
 
-        document.getElementById('result-section').innerHTML = html;
+// Clear existing content
+resultSection.innerHTML = '';
+
+response.meals.forEach(element => {
+    // Create main container div
+    const cardContainer = document.createElement('div');
+    cardContainer.classList.add('card');
+
+    // Create anchor element
+    const anchorElement = document.createElement('a');
+    anchorElement.href = `./meal-detail-page.html?id=${element.idMeal}`;
+    anchorElement.target = '_blank';
+
+    // Create card-top div
+    const cardTop = document.createElement('div');
+    cardTop.classList.add('card-top');
+
+    // Create dish-photo div
+    const dishPhoto = document.createElement('div');
+    dishPhoto.classList.add('dish-photo');
+    const img = document.createElement('img');
+    img.src = element.strMealThumb;
+    img.alt = '';
+    dishPhoto.appendChild(img);
+
+    // Create dish-name div
+    const dishName = document.createElement('div');
+    dishName.classList.add('dish-name');
+    dishName.textContent = element.strMeal;
+
+    // Create dish-details div
+    const dishDetails = document.createElement('div');
+    dishDetails.classList.add('dish-details');
+    dishDetails.innerHTML = `
+        Category: ${element.strCategory} <br>
+        Native: ${element.strArea} <br>
+        Tags: ${element.strTags}
+    `;
+
+    // Append dish-photo, dish-name, and dish-details to card-top
+    cardTop.appendChild(dishPhoto);
+    cardTop.appendChild(dishName);
+    cardTop.appendChild(dishDetails);
+
+    // Append card-top to anchor element
+    anchorElement.appendChild(cardTop);
+
+    // Create card-bottom div
+    const cardBottom = document.createElement('div');
+    cardBottom.classList.add('card-bottom');
+
+    // Create like div
+    const likeDiv = document.createElement('button');
+    likeDiv.id = 'like-button';
+    likeDiv.addEventListener('click', () => addRemoveToFavList(element.idMeal, likeDiv));
+    likeDiv.innerHTML = isFav(list, element.idMeal) ? 'Remove From Favourite' : 'Add To Favourite';
+
+
+    // Create play div
+    const playDiv = document.createElement('div');
+playDiv.classList.add('play');
+
+playDiv.innerHTML = `
+    <a href="${element.strYoutube}" target="_blank">
+    <svg xmlns="http://www.w3.org/2000/svg" height="16" width="18" viewBox="0 0 576 512"><!--!Font Awesome Free 6.5.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2023 Fonticons, Inc.--><path fill="#f10909" d="M549.7 124.1c-6.3-23.7-24.8-42.3-48.3-48.6C458.8 64 288 64 288 64S117.2 64 74.6 75.5c-23.5 6.3-42 24.9-48.3 48.6-11.4 42.9-11.4 132.3-11.4 132.3s0 89.4 11.4 132.3c6.3 23.7 24.8 41.5 48.3 47.8C117.2 448 288 448 288 448s170.8 0 213.4-11.5c23.5-6.3 42-24.2 48.3-47.8 11.4-42.9 11.4-132.3 11.4-132.3s0-89.4-11.4-132.3zm-317.5 213.5V175.2l142.7 81.2-142.7 81.2z"/></svg>
+    </a>
+`;
+
+// Now, append playDiv to your document or another container
+document.body.appendChild(playDiv);
+
+
+    // Append likeDiv and playDiv to card-bottom
+    cardBottom.appendChild(likeDiv);
+    cardBottom.appendChild(playDiv);
+
+    // Append anchorElement and card-bottom to cardContainer
+    cardContainer.appendChild(anchorElement);
+    cardContainer.appendChild(cardBottom);
+
+    // Append cardContainer to resultSection
+    resultSection.appendChild(cardContainer);
+});
+
     }
 }
 
 
-searchInput.addEventListener("input", (e) => {
-   noMealToShow() ;
-})
-
-window.addEventListener("load", (e) => {
-    noMealToShow() ;
-})
 
 
 
+if (searchInput) {
+    searchInput.addEventListener("input", (e) => {
+        noMealToShow();
+    });
+
+    window.addEventListener("load", (e) => {
+        noMealToShow();
+    });
+} else {
+    console.error("Search input not found!");
+}
